32f9c91927f4b53fb8f5a1d9b5ef0d6da79ee81d
Tarishi Geetey <40532748+tarishiG@users.noreply.github.com>
2019-04-30 00:50:52 +0530

    Add files via upload
    Music is generated on one tap over the different palet colors and animated bubble beautifies the sound view.

---
+++ MusicGenerator/BeCreative/index.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = ["#581845", "#900C3F", "#C70039", "#FF5733", "#FFC30F", "#49BEB7 "];
+
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function() {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      createBubble(index);
+    });
+  });
+
+  const createBubble = index => {
+    //Create bubbles
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = `jump 1s ease`;
+    bubble.addEventListener("animationend", function() {
+      visual.removeChild(this);
+    });
+  };
+});
